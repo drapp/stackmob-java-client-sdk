8726d83b7bf0c83bbb1b7888cb9f1a291e587216
Douglas Rapp <drapp@stackmob.com>
2012-05-04 10:52:25 -0700

    Use the first created StackMob as the shared one. We need to be able to get ahold of a session in StackMobApi, and if the shared StackMob wasn't set we were running into problems

--- src/main/java/com/stackmob/sdk/api/StackMob.java
+++ src/main/java/com/stackmob/sdk/api/StackMob.java
@@ -146,6 +146,7 @@ public class StackMob {
     public StackMob(String apiKey, String apiSecret, String userObjectName, String appName, Integer apiVersionNumber) {
         this.session = new StackMobSession(apiKey, apiSecret, userObjectName, appName, apiVersionNumber);
         this.executor = createNewExecutor();
+        if(StackMob.getStackMob() == null) StackMob.setStackMob(this);
     }
 
     /**
